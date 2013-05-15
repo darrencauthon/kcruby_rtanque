59177820945177a2c05d309126f3198641eefb08
Darren Cauthon <darren@cauthon.com>
2013-05-14 23:05:07 -0500

    Initial commit.

---
+++ bots/my_deadly_bot.rb
@@ -0,0 +1,17 @@
+class MyDeadlyBot < RTanque::Bot::Brain
+  NAME = 'darren'
+  include RTanque::Bot::BrainHelper
+
+  def tick!
+    ## main logic goes here
+    
+    # use self.sensors to detect things
+    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Sensors
+    
+    # use self.command to control tank
+    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Command
+    
+    # self.arena contains the dimensions of the arena
+    # See http://rubydoc.info/github/awilliams/RTanque/master/frames/RTanque/Arena
+  end
+end
