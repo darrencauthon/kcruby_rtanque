b9b769390bf874b0776df71e8ebf0676dbcb0e24
Darren Cauthon <darren@cauthon.com>
2013-06-23 15:20:28 -0500

    Move the code I will change to the bottom of the page.

--- bots/darren.rb
+++ bots/darren.rb
@@ -49,62 +49,63 @@ class Darren < RTanque::Bot::Brain
     end
   end
 
-  class ICantSeeAnybody < Strategy
-    def is_applicable?
-      bots.count == 0
-    end
-
-    def turn
-      command.fire_power = MIN_FIRE_POWER
-      spin_the_radar_in_a_circle
-    end
-
-    def spin_the_radar_in_a_circle
-      @degree ||= 0
-      @degree += 5
-      @degree = 0 if @degree > 360
-      command.radar_heading = RTanque::Heading.new_from_degrees @degree
-    end
-  end
-
-  class ISeeSomethingToShoot < Strategy
-
-    def is_applicable?
-      bots.count > 0
-    end
-
-    def turn
-      bot = bots.first
-
-      if @hit_a_wall == false && sensors.position.on_wall?
-        @direction = (@direction != :forward) ? :forward : :backward
-        @hit_a_wall = true
-      end
-      @hit_a_wall = false unless sensors.position.on_wall?
-
-      speed = MAX_BOT_SPEED
-      command.speed = @direction == :forward ? speed : -1 * speed
-
-      command.heading        = bot.heading + 95
-      command.radar_heading  = bot.heading
-      command.turret_heading = bot.heading
-
-      fire_power = MIN_FIRE_POWER
-      if headings_are_the_same sensors.turret_heading, bot.heading
-        fire_power = MAX_FIRE_POWER 
-      end
-      command.fire fire_power
-    end
-
-    def headings_are_the_same one, two
-      one = (one.to_degrees * 5).to_i
-      two = (two.to_degrees * 5).to_i
-      one == two
-    end
-
-  end
-
   def tick!
     Darren::Strategy.execute self
   end
 end
+
+class ICantSeeAnybody < Darren::Strategy
+  def is_applicable?
+    bots.count == 0
+  end
+
+  def turn
+    command.fire_power = MIN_FIRE_POWER
+    spin_the_radar_in_a_circle
+  end
+
+  def spin_the_radar_in_a_circle
+    @degree ||= 0
+    @degree += 5
+    @degree = 0 if @degree > 360
+    command.radar_heading = RTanque::Heading.new_from_degrees @degree
+  end
+end
+
+class ISeeSomethingToShoot < Darren::Strategy
+
+  def is_applicable?
+    bots.count > 0
+  end
+
+  def turn
+    bot = bots.first
+
+    if @hit_a_wall == false && sensors.position.on_wall?
+      @direction = (@direction != :forward) ? :forward : :backward
+      @hit_a_wall = true
+    end
+    @hit_a_wall = false unless sensors.position.on_wall?
+
+    speed = MAX_BOT_SPEED
+    command.speed = @direction == :forward ? speed : -1 * speed
+
+    command.heading        = bot.heading + 95
+    command.radar_heading  = bot.heading
+    command.turret_heading = bot.heading
+
+    fire_power = MIN_FIRE_POWER
+    if headings_are_the_same sensors.turret_heading, bot.heading
+      fire_power = MAX_FIRE_POWER 
+    end
+    command.fire fire_power
+  end
+
+  def headings_are_the_same one, two
+    one = (one.to_degrees * 5).to_i
+    two = (two.to_degrees * 5).to_i
+    one == two
+  end
+
+end
+
