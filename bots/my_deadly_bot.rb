690c5de2c23a825b157d7a3fc115d0c8695597b3
Darren Cauthon <darren@cauthon.com>
2013-06-23 13:17:52 -0500

    Reconfigure the bot to stop before it runs over the other tank.

--- bots/my_deadly_bot.rb
+++ bots/my_deadly_bot.rb
@@ -7,18 +7,28 @@ class MyDeadlyBot < RTanque::Bot::Brain
 
     command.fire MIN_FIRE_POWER
     
-    sensors.radar.each do |scanned_bot|
-      puts scanned_bot.inspect
-      command.heading = scanned_bot.heading
-      command.speed = 3
-      command.radar_heading = command.heading
-      command.turret_heading = command.heading
+
+    bot = bots_by_distance.first
+    return unless bot
+
+    if bot.distance < 300
+      command.speed = 0
+    else
+      command.speed = MAX_BOT_SPEED
     end
+
+    command.heading        = bot.heading
+    command.radar_heading  = command.heading
+    command.turret_heading = command.heading
+  end
+
+  def bots_by_distance
+    sensors.radar.sort { |x| x.distance }
   end
 
   def spin_the_radar_in_a_circle
     @degree ||= 0
-    @degree += 10
+    @degree += 5
     @degree = 0 if @degree > 360
     command.radar_heading = RTanque::Heading.new_from_degrees @degree
   end
