74dd3737bd41ece09104b9846383aff26e079eda
Darren Cauthon <darren@cauthon.com>
2013-06-23 21:48:04 -0500

    Do not get aimless tank stuck on a wall.

--- bots/darren.rb
+++ bots/darren.rb
@@ -93,7 +93,14 @@ class ICantSeeAnybody < Darren::Strategy
   def apply
     command.fire_power = MIN_FIRE_POWER
     spin_the_radar_in_a_circle
-    command.speed = 1
+    command.speed = speed
+  end
+
+  def speed
+    if hit_a_wall
+      @direction = (@direction != :forward) ? :forward : :backward 
+    end
+    @direction == :forward ? MAX_BOT_SPEED : -1 * MAX_BOT_SPEED
   end
 
   def spin_the_radar_in_a_circle
